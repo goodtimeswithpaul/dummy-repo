c86d209a4a23370a56516617000c264e2ca9a5a4
goodtimeswithpaul <moi@paultissot.ch>
2024-02-14 09:51:34 +0100

    hello

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        //This is a comment 2
+        //This is a comment 3
         System.out.println("Hello group 25!");
     }
 }
