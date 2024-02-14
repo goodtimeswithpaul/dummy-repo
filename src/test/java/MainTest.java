87dcc8d5fa018ebf7002392995b0d30c2d54acf2
Rikard Johansson <rikard01j@gmail.com>
2024-02-14 09:31:30 +0100

    Init commit

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+    @Test
+    void test() {
+        assertEquals(true, !false);
+    }
+
+    @Test
+    void test1() {
+        assertTrue(!false);
+    }
+}
